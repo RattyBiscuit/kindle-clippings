a589b98a2b1cc671334c12ed4f0acdc3ca1eba44
Alan Bellingham <ambellingham@gmail.com>
2025-09-14 00:22:58 +0100

    Refactor clippings parsing into ClippingsReader class
    - Introduces `ClippingsReader` class to encapsulate clippings loading, parsing, and grouping logic.
    - Moves file reading and parsing logic into `ClippingsReader` methods for better organization.
    - Adds parsing of date strings to datetime objects using `dateutil.parser`.
    - Implements logic to drop clippings that exceed the clipping limit.
    - Groups clippings by title and author after parsing.
    - Adds `make_markdown` method to generate markdown output for clippings.
    - Removes global variables and replaces them with instance variables within the `ClippingsReader` class.

--- runner.py
+++ runner.py
@@ -1,5 +1,6 @@
 import json
 
+import dateutil.parser
 from anyascii import anyascii
 from clippy import Clipping, Clippings
 
@@ -16,42 +17,86 @@ class Clipping(Clipping):
         title_author = super()._get_title_author(split_line)
         return self.settings["renames"].get(title_author, title_author)
 
+    def _get_date_added(self, split_line):
+        date_added = super()._get_date_added(split_line)
+        if date_added:
+            py_date = dateutil.parser.parse(date_added)
+            return py_date
+        return date_added
 
-with open("settings.json") as f:
-    settings = json.load(f)
 
-# Load raw clippings from a file
-with open("My Clippings.txt", "r", encoding="utf-8") as file:
-    raw_clippings = file.read().split("==========")
+class ClippingsReader:
+    def __init__(self, clippings_file="My Clippings.txt"):
+        self.clippings_file = clippings_file
+        self.clippings = Clippings()
+        self.clippings_by_title_author = {}
+        self.__read_settings()
+        self.__load_clippings_file()
 
+    def __read_settings(self):
+        with open("settings.json") as f:
+            self.settings = json.load(f)
 
-# Parse clippings
-clippings = Clippings()
-for raw_clipping in raw_clippings:
-    if raw_clipping.strip():  # Skip empty clippings
-        this_clipping = Clipping(raw_clipping, settings)
-        drop_clipping = False
-        if not this_clipping.text:
-            drop_clipping = True
-        if this_clipping.title_author in settings["drops"]:
-            for drop in settings["drops"][this_clipping.title_author]:
-                if (
-                    int(drop["start"]) == this_clipping.start_location
-                    and int(drop["end"]) == this_clipping.end_location
+    def __load_clippings_file(self):
+        # Load raw clippings from a file
+        with open("My Clippings.txt", "r", encoding="utf-8") as file:
+            self.raw_clippings = file.read().split("==========")
+
+    def parse(self):
+        self.__parse_clippings()
+        self.__group_clippings()
+
+    def __parse_clippings(self):
+        # Parse clippings
+        for raw_clipping in self.raw_clippings:
+            if raw_clipping.strip():  # Skip empty clippings
+                clipping = Clipping(raw_clipping, self.settings)
+                drop_clipping = False
+                if not clipping.text:
+                    drop_clipping = True
+                elif (
+                    "<You have reached the clipping limit for this item>"
+                    in clipping.text
                 ):
                     drop_clipping = True
-                    break
-        if not drop_clipping:
-            clippings.add_clipping(this_clipping)
+                if clipping.title_author in self.settings["drops"]:
+                    for drop in self.settings["drops"][clipping.title_author]:
+                        if (
+                            int(drop["start"]) == clipping.start_location
+                            and int(drop["end"]) == clipping.end_location
+                        ):
+                            drop_clipping = True
+                            break
+                if not drop_clipping:
+                    self.clippings.add_clipping(clipping)
 
-titles = set()
+    def __group_clippings(self):
+        clippings = sorted(self.clippings.clippings, key=lambda c: c.date)
+        for clipping in clippings:
+            self.clippings_by_title_author.setdefault(clipping.title_author, [])
+            self.clippings_by_title_author[clipping.title_author].append(clipping)
 
-for clipping in clippings.clippings:
-    if clipping.title_author == "ESV Bible":
-        print(clipping)
-    titles.add(clipping.title_author)
-print("")
-for title in sorted(titles):
-    print(title)
-print("")
-1
+    def make_markdown(self):
+        for title_author, clippings in self.clippings_by_title_author.items():
+            if not title_author.startswith("Lawrence"):
+                continue
+            self._make_markdown(title_author, clippings)
+            break
+
+    def _make_markdown(self, title_author, clippings):
+        print(title_author)
+        clips = []
+        for clipping in clippings:
+            clip_text = [
+                clipping.text,
+                f'<div style="text-align: right">Page {clipping.page} (Location {clipping.start_location}-{clipping.end_location})</div>',
+                "",
+            ]
+            clips.append("\n".join(clip_text))
+        print("\n---\n\n".join(clips))
+        return clips
+
+
+reader = ClippingsReader()
+reader.parse()
+reader.make_markdown()
